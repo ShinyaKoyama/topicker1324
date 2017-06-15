6ac4db355eb9429f5894868f701037bed47f71ee
shinya.koyama <shinya.koyama@axas-japan.co.jp>
2017-06-15 12:40:05 +0900

    [FIX] Mac使いたい。第2弾。

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -2,7 +2,7 @@
 $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
-        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit//',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit////',
         'Bake' => $baseDir . '/vendor/cakephp/bake/'
     ]
 ];
